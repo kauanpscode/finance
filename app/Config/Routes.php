082c8dc373e5653613fa9c00258aa74c8b66def7
kauanps <kauanps2271@gmail.com>
2025-10-06 07:29:59 -0300

    06-10-2025

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
 */
 
+$routes->setDefaultController('Access');
 $routes->setDefaultMethod('index');
-$routes->setDefaultController('Access::index');
-$routes->setAutoRoute(true); 
+$routes->setAutoRoute(true);
 
 $routes->get('/Main', 'Main::index');
