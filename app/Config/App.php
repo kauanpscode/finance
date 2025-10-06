039c0f94eef465467bdb5bc4473cf220a9ab538f
kauanps <kauanps2271@gmail.com>
2025-10-06 07:32:20 -0300

    06-10-2025

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.14.202/finance/';
+    public string $baseURL = 'http://finance.local';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
